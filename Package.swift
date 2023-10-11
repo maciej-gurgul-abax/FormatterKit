aa59068585295bbbb46bc5cbc16dda2fd19169db
Maciej Gurgul <maciej.gurgul@abax.com>
2023-10-11 23:50:10 +0200

    udate Package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FormatterKit",
     defaultLocalization: "en",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
